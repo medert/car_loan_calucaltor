c96f906470f4d3c4b8e00c9bff75fc48f935ff26
Meder <m.toktosunov@icloud.com>
2016-02-27 15:58:11 -0500

    added short instructions

--- car_loan.cpp
+++ car_loan.cpp
@@ -4,27 +4,31 @@
 int main(void) {
     double annual_rate, downpayment, price;
     double P, i, n, payment;
-    
+
+    printf("This program calculates the monthly payments of car loan");
+
     printf("Please, enter purchase price\n");
-    scanf("%lf", &price);
-    
+    scanf_s("%lf", &price);
+
     printf("Please, enter downpayment\n");
-    scanf("%lf", &downpayment);
-    
+    scanf_s("%lf", &downpayment);
+
     printf("Please, enter annual interest rate in percentage\n");
-    scanf("%lf", &annual_rate);
-    
+    scanf_s("%lf", &annual_rate);
+
     printf("Please, enter number of monthly payments\n");
-    scanf("%lf", &n);
-    
+    scanf_s("%lf", &n);
+
     P = price - downpayment;
-    
+
     i = annual_rate / 100 / 12;
-    
+
     payment =  (P * i) / (1 - pow(1+i, - n));
-    
+
     printf("The borrowed amount is $%.2f\n", P);
     printf("The monthly payment is $%.2f\n", payment);
-    
+
+    getchar();
+    getchar();
     return(0);
 }
